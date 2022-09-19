297b87bea6e47e50475b6db5b89fe64681b71f22
Rezar Dalipaj <rezar.dalipaj@fti.edu.al>
2022-09-19 09:55:55 +0200

    Test ok

--- src/test/java/com/example/singleendpoint/integration/ThenSomeOutcome.java
+++ src/test/java/com/example/singleendpoint/integration/ThenSomeOutcome.java
@@ -5,13 +5,13 @@ import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @JGivenStage
 public class ThenSomeOutcome extends Stage<ThenSomeOutcome> {
     @ExpectedScenarioState
     private ResultActions requestResult;
+
     public ThenSomeOutcome status_ok() throws Exception {
         requestResult.andExpect(status().isOk());
         return self();
